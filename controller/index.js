9e04124879ccd9443dd1b71ee9fae19858603115
illuminxry <secret.ian0926@gmail.com>
2023-12-02 16:58:56 +0800

    first commit

---
+++ controller/index.js
@@ -0,0 +1,59 @@
+const mysql = require("mysql");
+
+const conn = {
+    host: 'localhost',
+    database: 'uploadfile',
+    user: 'root',
+    password: ''
+};
+
+exports.getIndexPage = (req, res) => {
+    res.render('index');
+};
+const fs = require('fs');
+const path = require('path');
+
+exports.uploadfile = (req, res) => {
+    if (!req.file) {
+        res.status(400).send('No file uploaded');
+        return;
+    }
+
+    const connection = mysql.createPool(conn);
+
+    const filePath = req.file.path; // Assuming this is the path to the uploaded file
+    const fileStream = fs.createReadStream(filePath); // Create a readable stream for the file
+
+    let fileData = [];
+
+    fileStream.on('data', (chunk) => {
+        fileData.push(chunk);
+    });
+
+    fileStream.on('end', () => {
+        fileData = Buffer.concat(fileData); // Concatenate all chunks into a single buffer
+
+        connection.getConnection((err, connection) => {
+            if (err) {
+                console.error('Error getting MySQL connection:', err);
+                res.status(500).send('Error getting MySQL connection');
+                return;
+            }
+
+            const sql = 'INSERT INTO files (file) VALUES (?)';
+
+            connection.query(sql, [fileData], (error, results) => {
+                if (error) {
+                    console.error('Error inserting file data:', error);
+                    res.status(500).send('Error inserting file data');
+                    return;
+                }
+
+                res.status(200).send('File uploaded successfully');
+                connection.release(); // Release the connection back to the pool
+            });
+        });
+    });
+};
+
+    
---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+const indexrouter = require('../controller/index'); // Import the controller
+const multer = require('multer'); // Import multer
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+      callback(null, './uploads');
+  },
+  filename: (req, file, callback) => {
+      callback(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  },
+});
+const upload = multer({ storage: storage }); // Create the upload middleware
+/* GET home page. */
+router.get('/', indexrouter.getIndexPage);
+router.post('/upload', upload.single('file'), indexrouter.uploadfile);
+// router.get('/download/:id', indexrouter.downloadfile);
+
+module.exports = router;
